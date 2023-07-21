6e60a90c66d6b6d6c86ef9124ac6024130013919
Bhagat, Shubham Narendra <snbhagat@iu.edu>
2023-07-21 12:50:28 -0400

    added secret token validation to revalidate api

--- pages/api/revalidate/analytics.js
+++ pages/api/revalidate/analytics.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
   // // Check for secret to confirm this is a valid request
-  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid token' })
-  // }
+  if (req.query.secret !== process.env.NEXT_BASE_REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 
   try {
     // this should be the actual path not a rewritten path
